bf11d06370a969ef8873d086afc1a8306b443ae2
Siddharth Vadgama <me@siddv.net>
2018-08-16 21:56:30 +0100

    fix(List): 🎶 unbreak my tests 🎶

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,25 +1,36 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import List from '@/components/List.vue';
 import Card from '@/components/Card.vue';
 
+import storeConfig from '../store.config';
+
+Vue.use(Vuex);
+
 describe('List.vue', () => {
-  const name = 'potato';
+  let name;
+  let store;
+
+  beforeEach(() => {
+    name = 'potato';
+    store = new Vuex.Store(storeConfig);
+  });
 
   it('renders a name when passed one', () => {
     const wrapper = shallowMount(List, {
       propsData: { name },
+      store,
     });
     expect(wrapper.text()).to.include(name);
   });
 
   it('renders the correct number of Cards', () => {
-    const cards = [
-      { name: 'potato' },
-      { name: 'potatwo' },
-    ];
+    const id = 1;
     const wrapper = shallowMount(List, {
-      propsData: { name, cards },
+      propsData: { name, id },
+      store,
     });
     expect(wrapper.findAll(Card)).to.have.lengthOf(2);
   });
