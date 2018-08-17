88795f0bcf0cee3fd621616b9530b0d07c1f8981
Siddharth Vadgama <me@siddv.net>
2018-08-17 08:53:17 +0100

    refactor(store): add better default state

--- src/store/store.js
+++ src/store/store.js
@@ -17,33 +17,14 @@ export default new Vuex.Store({
     lists: [
       {
         id: 1,
-        name: 'List 1',
-      },
-      {
-        id: 2,
-        name: 'List 2',
+        name: 'An example list',
       },
     ],
     cards: [
       {
         id: 1,
         list: 1,
-        name: 'Card 1',
-      },
-      {
-        id: 2,
-        list: 1,
-        name: 'Card 2',
-      },
-      {
-        id: 3,
-        list: 2,
-        name: 'Card 3',
-      },
-      {
-        id: 4,
-        list: 2,
-        name: 'Card 4',
+        name: 'An example card',
       },
     ],
   },
